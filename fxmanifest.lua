1d9e28a0c04f916872ec969bbe35c41576923b54
Ukokavopic <55021493+uskokavopic@users.noreply.github.com>
2026-03-12 10:52:35 +0100

    Update fxmanifest.lua version to 1.0.2
    Updated version number to 1.0.2 and adjusted formatting.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,12 +3,12 @@ game 'gta5'
 
 author 'Uskokavopic'
 description 'OneSync-friendly ped script'
-version '1.0.0'
+version '1.0.2'
+
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
 
 client_scripts {
     'config.lua',
     'client.lua'
 }
-
-lua54 'yes'
-use_experimental_fxv2_oal 'yes'
